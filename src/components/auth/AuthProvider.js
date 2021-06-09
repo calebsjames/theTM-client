fc9b6e6fa94f57aae06fecc1f0def111519844e1
Caleb James <calebsjames@gmail.com>
2021-06-09 14:02:35 -0500

    build: basic auth functions

---
+++ src/components/auth/AuthProvider.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react"
+
+export const UserContext = React.createContext()
+
+export const UserProvider = (props) => {
+    const [user, setUser] = useState({events:[]})
+
+    const getUser = () => {
+        return fetch("http://localhost:8000/users", {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("tm_user_id")}`
+            }
+        })
+            .then(response => response.json())
+            .then(setUser)
+    }
+
+    return (
+        <UserContext.Provider value={{
+            user, getUser
+        }}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
---
+++ src/components/shows/ShowList.js
@@ -0,0 +1,52 @@
+export const ShowList = () => {
+    // const { getPosts, posts, searchTerms } = useContext(PostContext)
+    // const { getUsers, users } = useContext(UserContext)
+    // const { getCategories, categories } = useContext(CategoryContext)
+    // const { tags, getTags, getPostTags } = useContext(TagContext)
+    
+
+    // // useState to return filtered posts
+    // const [ filteredPosts, setFiltered ] = useState([])
+    // const history = useHistory()
+  
+    
+    // //state variable for post-tags
+    // const [postTags, setPostTags] = useState([])
+
+    // //state variable for sorting posts
+    
+
+    // // Initialization effect hook -> Go get post data
+    // useEffect(()=>{
+    //   getPosts().then(setFiltered)
+    //   getUsers()
+    //   getCategories()
+    //   getTags()
+    //   getPostTags().then(setPostTags)
+    // }, [])
+    // // console.log(posts)
+
+    // useEffect(() => {
+    //   if (searchTerms !== "") {
+    //     // console.log(searchTerms)
+    //     // If the search field is not blank, display matching posts
+    //     const subset = posts.filter(post => post.title.toLowerCase().includes(searchTerms.toLowerCase()))
+    //     setFiltered(subset)
+    //     // console.log("!", subset)
+    //   } else {
+    //     // If the search field is blank, display all posts
+    //     setFiltered(posts)
+    //   }
+    // }, [searchTerms, posts])
+
+
+
+
+
+      return (
+        <>
+            <h1>Shows</h1>
+            
+        </>
+    )
+}
